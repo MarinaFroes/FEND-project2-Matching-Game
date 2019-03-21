b5327177761298b45b7f7be2aeacc11c7be5895b
MFAC <ninafroes@gmail.com>
2019-03-21 14:29:38 +0100

    "add open cards function"

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
-//Timer functionality
+//TODO: Start by building a grid of randomly placed cards
+
+//TODO: Add the functionality to handle clicks (event delegation)
+function openCard() {
+  if (event.target.nodeName === 'LI') {
+    event.target.classList.add('open');
+  }
+}
+
+//TODO: Work on the matching logic
+
+
+//TODO: Create the winning condition
+
+
+//Implement additional functionality
+//TODO: Move counter
+
+
+//TODO: Timer
 let sec = 0;
 let min = 0;
 
@@ -14,5 +33,10 @@ function timer() {
     document.getElementById('timer').innerText = `End`;
   }
 }
-
 // const interval = setInterval(timer, 1000);
+
+//TODO: Star rating
+
+
+//TODO: Reset Button
+
